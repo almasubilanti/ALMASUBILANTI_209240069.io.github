2d64e1564926111f333cef0b2bee4f2de9aaaaa0
almasubilanti <alma3781264@gmail.com>
2025-07-12 17:16:47 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+// JS opsional, contoh alert sudah ada di produk.html
+function showAlert() {
+    alert('Website E-Commerce Sederhana Berjalan!');
+}
+
+// Memanggil fungsi untuk menunjukkan alert
+showAlert();
+
+// Menampilkan pesan di konsol
+console.log('Website E-Commerce Sederhana Berjalan!');
